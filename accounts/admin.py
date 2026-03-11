bd3e16bc323617866ed33bbac748837454c6e984
officialontar <officialontar@gmail.com>
2026-03-11 15:58:06 +0600

    Complete Django authentication project Class 48

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, RecruiterProfile, JobSeekerProfile
 
 
 
 # Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(RecruiterProfile)
+admin.site.register(JobSeekerProfile)
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'username', 'email', 'phone', 'role', 'is_staff', 'is_active')
-    list_filter = ('role', 'is_staff', 'is_active')
-    search_fields = ('username', 'email', 'phone', 'role')
-    ordering = ('id',)
